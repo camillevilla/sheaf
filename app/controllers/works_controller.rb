4ccc6764e7fd0d77eaf2270df24b486b9c8cd64e
Camille Villa <cvilla100@gmail.com>
2016-09-05 13:14:35 -0700

    Add views and routes for adding new publications and new copies

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -25,3 +25,33 @@ get '/works/:id' do
   @owners = User.all
   erb :'/works/show'
 end
+
+get '/works/:id/publications' do
+  @work = Work.find(params[:id])
+  @publications = @work.publications
+  erb :'/publications/index'
+end
+
+get '/works/:id/publications/new' do
+  @work = Work.find(params[:id])
+  erb :'/publications/new'
+end
+
+post '/works/:id/publications' do
+  @publication = Publication.new(params[:publication])
+  @publication.work_id = params[:id]
+  if @publication.save
+    redirect "/works/#{params[:id]}/publications"
+  else
+    redirect "/works/#{params[:id]}/publications/new"
+  end
+end
+
+post '/works/:work_id/publications/:publication_id/copies' do
+  @publication = Publication.find(params[:publication_id])
+  @copy = Own.create(
+    user_id: session[:id],
+    publication_id: @publication.id
+  )
+  redirect "/works/#{params[:work_id]}"
+end
