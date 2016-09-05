a59d7428d056785154d50938f9355c92300efe7b
Camille Villa <cvilla100@gmail.com>
2016-09-05 09:14:33 -0700

    Add owner names to copies table

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -11,5 +11,6 @@ get '/works/:id' do
   @work = Work.find(params[:id])
   @publications = Work.find(params[:id]).publications
   @copies = Work.find(params[:id]).owns
+  @owners = User.all
   erb :'/works/show'
 end
