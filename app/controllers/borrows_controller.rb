540c6949f84765539d9d858c8ffce2e27eb12df0
Camille Villa <cvilla100@gmail.com>
2016-09-08 11:16:13 -0700

    Add book information to borrow request form

---
+++ app/controllers/borrows_controller.rb
@@ -0,0 +1,30 @@
+get '/owns/:id/borrows/new' do 
+  @copy = Own.find(params[:id])
+  @publication = @copy.publication
+  @work = @publication.work
+  @owner = @copy.user
+  @borrower = User.find(session[:id])
+  erb :'/borrows/new'
+end 
+
+post '/owns/:id/borrows/sms-request' do
+  @owner = Own.find(params[:id]).user
+  to = @owner.phone_number
+  @message = params[:request]
+  # to = params["to"]
+  # message = params["body"]
+
+  client = Twilio::REST::Client.new(
+    ENV["TWILIO_ACCOUNT_SID"],
+    ENV["TWILIO_AUTH_TOKEN"]
+    )
+
+  client.messages.create(
+    to: to,
+    from: ENV["TWILIO_PHONE_NUMBER"],
+    body: @message
+    )
+
+  # eventually redirect to a nicer success page
+  redirect "/users/#{session[:id]}"
+end
--- app/controllers/owns_controller.rb
+++
@@ -1,5 +0,0 @@
-get '/owns/:id/borrows/new' do 
-  @copy = Own.find(params[:id])
-  erb :'/borrows/new'
-end 
-
