a4d79f22bbde7fc195a82043baca078c1d4abd34
Camille Villa <cvilla100@gmail.com>
2016-09-08 23:57:18 -0700

    hide/show books borrowed and books lent

--- public/js/application.js
+++ public/js/application.js
@@ -7,6 +7,12 @@ var booksBorrowedListener = function(){
   $("#books-borrowed-toggle").on("click", function(event){
     event.preventDefault();
 
+    //hide if clicked again
+    if ($("#books-borrowed").length > 0){
+      $("#books-borrowed").remove();
+    }
+    else
+    {
     var targetUrl = $("#home").attr("href") + "/borrows"
 
     $.ajax({
@@ -21,12 +27,18 @@ var booksBorrowedListener = function(){
     .fail(function(error){
       console.log(error)
     });
-  })
+  }})
 }
 
 var booksLentListener = function(){
   $("#books-lent-toggle").on("click", function(event){
     event.preventDefault();
+
+    if ($("#books-lent").length > 0){
+      $("#books-lent").remove();
+    }
+    else
+    {
 
     var targetUrl = $("#home").attr("href") + "/books-lent"
 
@@ -42,5 +54,5 @@ var booksLentListener = function(){
     .fail(function(error){
       console.log(error)
     });
-  })
+  }})
 }
