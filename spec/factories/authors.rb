727a3ca7ee2e5be93da784caf8473b91b038452b
Camille Villa <cvilla100@gmail.com>
2016-09-18 23:43:53 -0700

    Add factories for all models

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -2,4 +2,41 @@ FactoryGirl.define do
   factory :author do
     name "Toni Morrison"
   end
+
+  factory :work do
+    title "Song of Solomon"
+    author
+  end
+
+  factory :publisher do
+    name "Alfred A. Knopf"
+  end
+
+  factory :format do
+    kind "print"
+  end
+
+  factory :edition do
+    work
+    publisher
+    publication_year 1977
+  format
+  end
+
+  factory :user, aliases: [:owner, :borrower] do
+    name "Tom"
+    email "tom@tom.com"
+    phone "11231231234"
+    password "password"
+  end
+
+  factory :copy do
+    owner
+    edition
+  end
+
+  factory :loan do
+    borrower
+    copy    
+  end
 end
