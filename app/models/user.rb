f16979c750ff837c05153045b947f85b67177842
cvilla100@gmail.com <cvilla100@gmail.com>
2016-10-09 22:18:22 -0700

    Add Devise controllers and views for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :validatable
   has_many :owned_books, foreign_key: "user_id", class_name: "Copy"
   # has_many :borrowed_books, foreign_key: "user_id", class_name: "Loan"
   # rewrite these associations after thinking through borrowing and lending term
