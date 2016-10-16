76a894753c563658437ac8b0cf549754bd6b7664
cvilla100@gmail.com <cvilla100@gmail.com>
2016-10-16 00:33:10 -0700

    Add controller, routes, and views for copies

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations:'users/registrations', sessions: 'users/sessions' }
+  
+  resources :users do
+    resources :copies
+  end
+
   resources :authors
   resources :works
   resources :publishers
   resources :editions
+  
   root "demo#welcome"
 end
