c43b6e340110316872c917e0246b8665365fdc82
cvilla100@gmail.com <cvilla100@gmail.com>
2016-10-15 18:44:46 -0700

    Add controller, routes, and views for Publisher

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations:'users/registrations', sessions: 'users/sessions' }
   resources :authors
   resources :works
+  resources :publishers
   root "demo#welcome"
 end
