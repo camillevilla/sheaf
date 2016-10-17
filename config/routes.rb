4dde24ba40e46207440606da4c3108fa0cfd0d0e
cvilla100@gmail.com <cvilla100@gmail.com>
2016-10-16 19:48:11 -0700

    Require authentication for all book-related resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations:'users/registrations', sessions: 'users/sessions' }
   
-  resources :users do
-    resources :copies
+  authenticate :user do
+    resources :users do
+      resources :copies
+    end
+    resources :authors
+    resources :works
+    resources :publishers
+    resources :editions
   end
-
-  resources :authors
-  resources :works
-  resources :publishers
-  resources :editions
   
   root "demo#welcome"
 end
