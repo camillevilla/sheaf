299cacc5f359b149f3648253472397a213599b8b
cvilla100@gmail.com <cvilla100@gmail.com>
2016-10-17 15:30:27 -0700

    User can send friend requests to registered users

---
+++ spec/features/adding_a_friend_spec.rb
@@ -0,0 +1,6 @@
+# Checklist
+
+# cannot send requests to emails that are not registered users
+# user cannot create multiple requests to the same user
+# cannot create pending reciprocal requests
+# cannot send request to users you're already friends with
