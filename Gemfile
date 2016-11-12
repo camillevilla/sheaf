df193ed1395a4539671be8381942833aaaf515d4
cvilla100@gmail.com <cvilla100@gmail.com>
2016-11-11 18:38:54 -0800

    Install Faker; refactor factories

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'devise'
 gem 'postmark-rails'
+gem 'faker'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
   factory :author do
-    name "Toni Morrison"
+    name {Faker::Book::author}
   end
 
   factory :work do
-    title "Song of Solomon"
+    title {Faker::Book::title}
     author
   end
 
   factory :publisher do
-    name "Alfred A. Knopf"
+    name {Faker::Book::publisher}
   end
 
   # factory :format do
@@ -23,11 +23,19 @@ FactoryGirl.define do
     format_type "paperback"
   end
 
-  factory :user, aliases: [:owner, :borrower] do
-    name "Tom"
-    email "tom@tom.com"
-    phone "11231231234"
-    password "password"
+  # rename this factory, create a second user
+  # factory :user, aliases: [:owner, :borrower] do
+  #   name "Tom"
+  #   email "tom@tom.com"
+  #   phone "11231231234"
+  #   password "password"
+  # end
+
+  factory :user, aliases: [:owner, :borrower] do |user|
+  name {Faker::Name::first_name}
+  email {Faker::Internet.email}
+  phone "11231231234"
+  password "password"
   end
 
   factory :copy do
@@ -37,6 +45,7 @@ FactoryGirl.define do
 
   factory :loan do
     borrower
-    copy    
+    copy
+    status_code 0
   end
 end
