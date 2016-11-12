f1ce93bddd09629a13fbecde936ff40b747f20b5
cvilla100@gmail.com <cvilla100@gmail.com>
2016-11-11 20:26:46 -0800

    Adds list of lent / borrowed books and return links

---
+++ app/controllers/loans_controller.rb
@@ -0,0 +1,66 @@
+class LoansController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  include TwilioHelper
+
+  def index
+    @borrowed_copies = Loan.where(borrower: current_user, status_code: 1)
+    @lent_copies = Loan.where(status_code:1).select{|a| a.owner.id == current_user.id}
+
+    # refactor this method later OR write a helper
+    @pending_requests = Loan.where(status_code:0).select{|a| a.owner.id == current_user.id}
+
+  end
+
+  def new
+    @copy = Copy.find(params[:copy_id])
+    @owner = @copy.owner
+  end
+
+  def create
+    @copy = Copy.find(params[:copy_id])
+    @owner = @copy.owner
+    recipient = @owner.phone
+    message = params[:request]
+
+    # send_sms(recipient, message)
+    client = Twilio::REST::Client.new(
+      ENV["TWILIO_ACCOUNT_SID"],
+      ENV["TWILIO_AUTH_TOKEN"]
+      )
+
+    client.messages.create(
+      to: recipient,
+      from: ENV["TWILIO_PHONE_NUMBER"],
+      body: message
+      )
+
+    # eventually redirect to a nicer success page
+    redirect_to user_copies_path(@owner)
+  end
+
+  def update
+    @loan = Loan.find(params[:id])
+    @loan.lend_copy
+    @loan.save
+    redirect_to loans_path
+  end
+
+  def destroy
+    @loan = Loan.find(params[:id])
+    @loan.return_copy
+    @loan.save
+    redirect_to loans_path
+  end
+
+  # def sms_request
+  #   @copy = Copy.find(params[:copy_id])
+  #   @owner = @copy.owner
+  #   recipient = @owner.phone
+  #   message = params[:request]
+
+  #   send_sms(recipient, message)
+
+  #   # eventually redirect to a nicer success page
+  #   redirect_to user_copies_path(@owner)
+  # end
+end
--- db/migrate/20160918013548_create_loans.rb
+++ db/migrate/20160918013548_create_loans.rb
@@ -3,7 +3,7 @@ class CreateLoans < ActiveRecord::Migration[5.0]
     create_table :loans do |t|
       t.references :user, foreign_key: true
       t.references :copy, foreign_key: true
-      t.integer :status_code # 0:pending, 1:out, 2:returned
+      t.integer :status_code # 0:pendingr request, 1: checked out, 2:returned 3:rejected
       t.date :checkout_date
       t.date :return_date
 
