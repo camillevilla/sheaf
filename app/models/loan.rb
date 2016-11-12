5f75edfc2821357bd6c70c8f29d6b86fc59ab8e8
cvilla100@gmail.com <cvilla100@gmail.com>
2016-11-11 18:40:40 -0800

    Write and pass tests for Loan model and methods

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,18 +1,18 @@
 class Loan < ApplicationRecord
   belongs_to :borrower, foreign_key: "user_id",class_name: "User"
   belongs_to :copy
-  # don't think this is working quite right...
-  # has_one :owner, through: :copy
   has_one :owner, through: :copy
 
   validates :copy, presence: true
   validates :borrower, presence: true
+  validates :owner, presence: true
+  validates :status_code, presence: true
 
   def lend_copy
-    Loan.find(id).update_attributes(status_code: 1)
+    update_attributes(status_code: 1)
   end
 
   def return_copy
-    Loan.find(id).update_attributes(status_code: 2)
+    update_attributes(status_code: 2)
   end
 end
