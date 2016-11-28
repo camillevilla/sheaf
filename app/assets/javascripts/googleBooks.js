199c958b8d5f9e9671c9356aab26110cb726f733
cvilla100@gmail.com <cvilla100@gmail.com>
2016-11-27 21:26:57 -0800

    Reset form betwen ISBN searches; add reset button

--- app/assets/javascripts/googleBooks.js
+++ app/assets/javascripts/googleBooks.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
+  resetLookupStatus();
+  resetButton();
   lookupByISBN13();  
-  $('#lookup-status').hide()
 });
 
 var lookupByISBN13 = function(){
   $("#lookupISBN").on('click', function(event){
 
   event.preventDefault();
+
+  resetLookupStatus();
 
   var googleBooksAPI = "https://www.googleapis.com/books/v1/volumes?q=";
   var isbn13Field = $('#editionISBN13');
@@ -23,12 +26,27 @@ $.getJSON(googleBooksAPI + 'isbn:' + isbn13,
         $('#editionPublicationyear').val(volumeInfo["publishedDate"]);
         $('#lookup-status').show()
         $('#lookup-status').addClass('btn-success')
-        $('#lookup-status').html('<i class="fa fa-check fa-lg" alt="success"></i>')
+        $('#lookup-status').html('<i class="fa fa-check fa-lg" alt="search successful"></i>')
       } else {
-        console.log('error')
         $('#lookup-status').show()
         $('#lookup-status').addClass('btn-danger')
-        $('#lookup-status').html('<i class="fa fa-times fa-lg" alt="success"></i>')
+        $('#lookup-status').html('<i class="fa fa-times fa-lg" alt="failed search"></i>')
+        $('#lookup-failure-message').show();
       }
-    });
-})};
+    }); //end $.getJSON
+  }) // end $("#lookupISBN").on
+}; // end lookupByISBN13
+
+// reset ISBN lookup form 
+var resetLookupStatus = function(){
+  $('#lookup-status').hide()
+  $('#lookup-failure-message').hide();
+  $('#lookup-status').removeClass('btn-success')
+  $('#lookup-status').removeClass('btn-danger')
+}
+
+var resetButton = function(){
+  $('button[type="reset"]').on('click', function(){
+    resetLookupStatus();
+  })
+}
