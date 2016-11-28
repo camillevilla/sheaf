43b848e483ab14774e6805bd762a4e5f503a4e70
cvilla100@gmail.com <cvilla100@gmail.com>
2016-11-28 01:29:19 -0800

    Add ISBN10 lookup to add book form

--- app/assets/javascripts/googleBooks.js
+++ app/assets/javascripts/googleBooks.js
@@ -1,29 +1,42 @@
-$(document).ready(function() {
-  resetLookupStatus();
-  resetButton();
-  lookupByISBN13();  
-});
+/* global $ */
+$(document).ready(function () {
+  $('#lookup-status').hide()
+  $('#lookup-failure-message').hide()
+  resetLookupStatus()
+  resetButton()
+  searchButton()
+  lookupByISBN()
+})
 
-var lookupByISBN13 = function(){
-  $("#lookupISBN").on('click', function(event){
+// Search by either ISBN13 or ISBN10
+var searchButton = function () {
+  $('#lookupISBN').on('click', function (event) {
+    event.preventDefault()
+    resetLookupStatus()
+    var isbn13 = $('#editionISBN13').val()
+    var isbn10 = $('#editionISBN10').val()
 
-  event.preventDefault();
+  // prevent search if both ISBN fields are empty
+    if (isbn13 === '') {
+      lookupByISBN(isbn10)
+    } else {
+      lookupByISBN(isbn13)
+    }
+  })
+}
 
-  resetLookupStatus();
-
-  var googleBooksAPI = "https://www.googleapis.com/books/v1/volumes?q=";
-  var isbn13Field = $('#editionISBN13');
-  var isbn13 = isbn13Field.val();
-
-$.getJSON(googleBooksAPI + 'isbn:' + isbn13,
-    function(data){
+// Google Books search by ISBN
+var lookupByISBN = function (isbn) {
+  var googleBooksAPI = 'https://www.googleapis.com/books/v1/volumes?q='
+  $.getJSON(googleBooksAPI + 'isbn:' + isbn,
+    function (data) {
       if (data.totalItems >= 1) {
-        var volumeInfo = data.items[0]["volumeInfo"]
+        var volumeInfo = data.items[0]['volumeInfo']
         console.log(volumeInfo)
-        $('#authorName').val(volumeInfo["authors"][0]);
-        $('#workTitle').val(volumeInfo["title"]);
-        $('#publisherName').val(volumeInfo["publisher"]);
-        $('#editionPublicationyear').val(volumeInfo["publishedDate"]);
+        $('#authorName').val(volumeInfo['authors'][0])
+        $('#workTitle').val(volumeInfo['title'])
+        $('#publisherName').val(volumeInfo['publisher'])
+        $('#editionPublicationyear').val(volumeInfo['publishedDate'])
         $('#lookup-status').show()
         $('#lookup-status').addClass('btn-success')
         $('#lookup-status').html('<i class="fa fa-check fa-lg" alt="search successful"></i>')
@@ -31,22 +44,21 @@ $.getJSON(googleBooksAPI + 'isbn:' + isbn13,
         $('#lookup-status').show()
         $('#lookup-status').addClass('btn-danger')
         $('#lookup-status').html('<i class="fa fa-times fa-lg" alt="failed search"></i>')
-        $('#lookup-failure-message').show();
+        $('#lookup-failure-message').show()
       }
-    }); //end $.getJSON
-  }) // end $("#lookupISBN").on
-}; // end lookupByISBN13
+    })
+} // end lookupByISBN
 
-// reset ISBN lookup form 
-var resetLookupStatus = function(){
+// reset ISBN lookup form
+var resetLookupStatus = function () {
   $('#lookup-status').hide()
-  $('#lookup-failure-message').hide();
+  $('#lookup-failure-message').hide()
   $('#lookup-status').removeClass('btn-success')
   $('#lookup-status').removeClass('btn-danger')
 }
 
-var resetButton = function(){
-  $('button[type="reset"]').on('click', function(){
-    resetLookupStatus();
+var resetButton = function () {
+  $('button[type="reset"]').on('click', function () {
+    resetLookupStatus()
   })
 }
