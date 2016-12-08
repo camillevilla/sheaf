a4c5fec3c63029fe5fad12e1626771481114ad84
cvilla100@gmail.com <cvilla100@gmail.com>
2016-12-08 14:40:36 -0800

    Replace format model with format code in Editions

--- db/migrate/20160918012801_create_formats.rb
+++
@@ -1,9 +0,0 @@
-class CreateFormats < ActiveRecord::Migration[5.0]
-  def change
-    create_table :formats do |t|
-      t.string :kind
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20160918012934_create_editions.rb
+++ db/migrate/20160918012934_create_editions.rb
@@ -2,13 +2,9 @@ class CreateEditions < ActiveRecord::Migration[5.0]
   def change
     create_table :editions do |t|
       t.references :work, foreign_key: true
-      # publisher should probably be an optional field
       t.references :publisher, foreign_key: true
       t.integer :publication_year
-      # add polymorphic for format types later
-      # t.references :format, foreign_key: true
-
-      t.string :format_type
+      t.integer :format_code # 0 - print, 1 - digital
       t.string :isbn10
       t.string :isbn13
 
