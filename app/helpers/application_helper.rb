3add453a4f6c4b91eeeecb3bdc5bd1e1680f96f9
cvilla100@gmail.com <cvilla100@gmail.com>
2017-01-24 16:52:26 -0800

    Add helper method for authorization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def friends?(user1_id, user2_id)
     User.find(user1_id).friends.include?(User.find(user2_id))
-  end  
+  end
+
+  def access_authorized?(user)
+    (user == current_user) || friends?(user.id, current_user.id)
+  end
 end
